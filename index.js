a89c259a413404abbff78124d40863d586c35544
Mohamad Shiralizadeh <mohamad.shir@gmail.com>
2022-01-07 20:01:19 +0400

    Demo changes

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import { of, map, combineLatest, BehaviorSubject } from "rxjs";
 
-const foo = of(2);
-const bar = of(100);
+const foo = new BehaviorSubject(2);
+const bar = new BehaviorSubject(100);
 
 const result = combineLatest([foo, bar]).pipe(
   map(([foo, bar]) => {
+    console.log("map");
     return foo * bar;
   })
 );
+
+result.subscribe((value) => {
+  console.log("subscribe");
+  console.log(value);
+});
+
+setTimeout(() => {
+  console.log("setTimeout");
+  foo.next(10);
+}, 5000);
+
+console.log("end");
